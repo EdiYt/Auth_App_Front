5d8d66937622b0c0a7e7ec96610b82ebbea1ed0d
Erick <erickvillaseor@yahoo.com.mx>
2025-08-01 17:42:02 -0600

    Se agrego navbar y botones

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,10 +2,11 @@ import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import {MatButtonModule} from '@angular/material/button'
+import { NavbarComponent } from "./components/navbar/navbar.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, CommonModule, MatButtonModule],
+  imports: [RouterOutlet, CommonModule, MatButtonModule, NavbarComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
