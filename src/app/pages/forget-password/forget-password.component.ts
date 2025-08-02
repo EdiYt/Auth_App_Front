fba59904b24ad603b5c01df6bb6b82c6bf3cbc91
Erick <erickvillaseor@yahoo.com.mx>
2025-08-02 15:10:52 -0600

    Se agrego la pagina por si olvidaste tu contraseña

---
+++ src/app/pages/forget-password/forget-password.component.ts
@@ -0,0 +1,46 @@
+import { Component, inject } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { AuthService } from '../../services/auth.service';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { HttpErrorResponse } from '@angular/common/http';
+import { MatIconModule } from '@angular/material/icon';
+
+@Component({
+  selector: 'app-forget-password',
+  imports: [FormsModule, MatIconModule],
+  templateUrl: './forget-password.component.html',
+  styleUrl: './forget-password.component.css'
+})
+export class ForgotPasswordComponent {
+  email!: string;
+  authService = inject(AuthService);
+  matSnackBar = inject(MatSnackBar);
+  showEmailSent = false;
+  isSubmitting = false;
+
+  forgetPassword() {
+    this.isSubmitting = true;
+    this.authService.forgotPassword(this.email).subscribe({
+      next: (response) => {
+        if (response.isSuccess) {
+          this.matSnackBar.open(response.message, 'Close', {
+            duration: 5000,
+          });
+          this.showEmailSent = true;
+        } else {
+          this.matSnackBar.open(response.message, 'Close', {
+            duration: 5000,
+          });
+        }
+      },
+      error: (error: HttpErrorResponse) => {
+        this.matSnackBar.open(error.message, 'Close', {
+          duration: 5000,
+        });
+      },
+      complete: () => {
+        this.isSubmitting = false;
+      },
+    });
+  }
+}
